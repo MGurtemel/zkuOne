1132bb2a54a82afcb4765fa5ec85a07959f52d0e
MGurtemel <88720478+MGurtemel@users.noreply.github.com>
2022-04-25 20:11:19 +0300

    Upload Storage.sol

---
+++ Storage.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Storage {
+
+    uint256 number;
+
+    /**
+     * @dev Store value in variable
+     * @param num value to store
+     */
+    function storeNumber(uint256 num) public {
+        number = num;
+    }
+
+    /**
+     * @dev Return value 
+     * @return value of 'number'
+     */
+    function retrieveNumber() public view returns (uint256) {
+        return number;
+    }
+
+    /**
+     * @dev Return string 
+     * @return 'Hello World'
+     */
+    function helloWorld() public pure returns (string memory) {
+        return "Hello World";
+    }
+}
